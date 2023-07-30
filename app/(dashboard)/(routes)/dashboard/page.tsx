7d734aa8f2af93ea7287257cca47efadba975a96
abdorabee <abdorabee1134@gmail.com>
2023-07-30 12:38:03 +0300

    folder setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const DashboardPage = () => {
+  return <Button>Press Me :)</Button>;
+};
+
+export default DashboardPage;
