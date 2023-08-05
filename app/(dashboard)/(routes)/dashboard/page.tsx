22f8721ccb49acdd5ab1f080a2dcbcfab9f2f72c
abdorabee <abdorabee1134@gmail.com>
2023-08-05 19:00:09 +0300

    finished the dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,93 @@
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+"use client";
+import { Card } from "@/components/ui/card";
+import {
+  Code,
+  VideoIcon,
+  ImageIcon,
+  MessageSquare,
+  ArrowRight,
+  MusicIcon,
+} from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+  {
+    label: "Music Generation",
+    icon: MusicIcon,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music ",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-500",
+    bgColor: "bg-pink-500/10",
+    href: "/image ",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/image ",
+  },
+  {
+    label: "Code",
+    icon: Code,
+    color: "text-red-700",
+    bgColor: "bg-red-700/10",
+    href: "/image ",
+  },
+];
 
 const DashboardPage = () => {
+  const router = useRouter();
   return (
     <div>
-      <h1>Dashboard Page (Protected)</h1>
+      <div className="mb-8 space-y-8">
+        <h2
+          className="text-2xl md:text-4xl font-bold 
+       text-center"
+        >
+          Explore the power of AI with Nathan
+        </h2>
+        <p
+          className="text-muted-foreground text-center font-light text-sm 
+        md:text-lg "
+        >
+          I hope you have a great experince with Nathan
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5
+             flex items-center
+              justify-between
+               hover:shadow-md
+                transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
