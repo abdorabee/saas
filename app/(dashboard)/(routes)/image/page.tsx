a674fdedd8bacdfd84f3af9b8b3f304f5ac590fc
abdorabee <abdorabee1134@gmail.com>
2023-08-10 15:34:39 +0300

    image generation integration pt1

---
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import axios from "axios";
+import { formSchema } from "./constants";
+import * as z from "zod";
+import { Heading } from "@/components/heading";
+import { ImageIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Empty from "@/components/empty";
+import Loader from "@/components/loader";
+import { cn } from "@/lib/utils";
+import { UserAvater } from "@/components/user-avater";
+import { BotAvater } from "@/components/bot-avater";
+
+const ImagePage = () => {
+  const router = useRouter();
+  const [images, setImages] = useState<string[]>([]);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+      amount: "1",
+      resolution: "512x512",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setImages([]);
+      const response = await axios.post("/api/image", values);
+      const urls = response.data.map((image: { url: string }) => image.url);
+      setImages(urls);
+      form.reset();
+    } catch (error) {
+      // TODO:OPEN pro model
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Image Generation"
+        description="This is the image generation page (beta)"
+        icon={ImageIcon}
+        iconColor="text-pink-700"
+        bgColor="bg-pink-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="
+            rounded-lg
+            border
+            w-full
+            p-4
+            px-3
+            md:px-6
+            grid-cols-12
+            gap-2
+            "
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="boder-0 outline-none
+                    focus-visible:ring-0
+                    focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="I am your AI assistant make sure to thank Abdel-Rahman for bringing me alive :)"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-20">
+              <Loader />
+            </div>
+          )}
+          {images.length === 0 && !isLoading && (
+            <Empty label="no images generated " />
+          )}
+          <div>Images will be rendered here</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImagePage;
